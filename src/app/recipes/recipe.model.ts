2b27e5346f233a9a27c3cf5bdf2f1964ad12ba33
Justina Kuchta <justyna.kuchta@gmail.com>
2020-04-24 15:07:24 -0400

    created a Recipe model

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+// a Typescript class is all we need to create a model
+export class Recipe {
+    public name: string
+    public description: string
+    public imagePath: string
+    
+    constructor(name: string, description: string, imagePath: string) {
+        this.name = name
+        this.description = description
+        this.imagePath = imagePath
+    }
+
+}
