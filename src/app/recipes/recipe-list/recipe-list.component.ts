ac44dcc26ea435d31e53c7f15a52ca653235b5e7
Justina Kuchta <justyna.kuchta@gmail.com>
2020-04-24 15:30:24 -0400

    used ngFor to loop through the recipes arr

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = []
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a testy test', 'https://img.hellofresh.com/f_auto,fl_lossy,q_auto,w_610/hellofresh_s3/image/mediterranean-hummus-couscous-bowls-9aa1b9c2.jpg')
+  ]
 
   constructor() { }
 
