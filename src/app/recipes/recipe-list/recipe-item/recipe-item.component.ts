2dd2cc82c4afdf122c59ea6ab4aff1a996ff0646
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-08 16:07:13 -0400

    added alternative solution

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,28 @@
+// import { Component, OnInit, Input } from '@angular/core';
+
+// @Component({
+//   selector: 'app-recipe-item',
+//   templateUrl: './recipe-item.component.html',
+//   styleUrls: ['./recipe-item.component.css']
+// })
+// export class RecipeItemComponent implements OnInit {
+
+
+//   @Input() name: string
+//   @Input() description: string
+//   @Input() src: string
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+
+// HIS SOLUTION:
+
 import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model'
 
 @Component({
   selector: 'app-recipe-item',
@@ -7,11 +31,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
 
-
-  @Input() name: string
-  @Input() description: string
-  @Input() src: string
-
+  @Input() recipe: Recipe
+  
   constructor() { }
 
   ngOnInit(): void {
