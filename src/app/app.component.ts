e5e7106f0322f4a2aa09702ab68b3f28259ba638
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-08 15:59:58 -0400

    recipe-item component takes data from recipe-list with ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title = 'recipe-book';
-  option;
+  option = 'recipes';
 
   onLinkClicked(data) {
     console.log("data", data)
