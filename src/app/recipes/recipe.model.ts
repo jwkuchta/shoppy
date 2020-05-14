3c4f2828e9a5d1899ec58d9f01cd03efcbbf6cd0
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-14 14:54:26 -0400

    added Ingredient to the Recipe model and service

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -13,6 +13,14 @@
 
 // }
 
+import { Ingredient } from '../shared/ingredient.model'
+
 export class Recipe {
-    constructor(public name: string, public description: string, public imagePath: string) {}
+    constructor(
+        public name: string, 
+        public description: string, 
+        public imagePath: string, 
+        public ingredients: Ingredient[]
+    ) 
+    {}
 }
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,13 +1,35 @@
 import { Recipe } from './recipe.model'
 import { EventEmitter } from '@angular/core'
+import { Ingredient } from '../shared/ingredient.model'
 
 export class RecipesService {
     recipeSelected = new EventEmitter<Recipe>()
 
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'This is a testy test', 'https://img.hellofresh.com/f_auto,fl_lossy,q_auto,w_610/hellofresh_s3/image/mediterranean-hummus-couscous-bowls-9aa1b9c2.jpg'),
-        new Recipe('Buttered Chinken', 'this is one of my favorites', 'https://cafedelites.com/wp-content/uploads/2019/01/Butter-Chicken-IMAGE-64.jpg'),
-        new Recipe('Mongolian Beef', 'Diego likes this one', 'https://omnivorescookbook.com/wp-content/uploads/2018/05/1805_Mongolian-Beef_550.jpg')
+        new Recipe(
+            'Test Recipe', 
+            'This is a testy test', 
+            'https://img.hellofresh.com/f_auto,fl_lossy,q_auto,w_610/hellofresh_s3/image/mediterranean-hummus-couscous-bowls-9aa1b9c2.jpg', 
+            [
+                new Ingredient('testy test', 1),
+                new Ingredient('testy sauce', 2)
+            ]),
+        new Recipe(
+            'Buttered Chinken', 
+            'this is one of my favorites', 
+            'https://cafedelites.com/wp-content/uploads/2019/01/Butter-Chicken-IMAGE-64.jpg', 
+            [
+                new Ingredient('chicken', 1),
+                new Ingredient('butter', 2)
+            ]),
+        new Recipe(
+            'Mongolian Beef', 
+            'Diego likes this one', 
+            'https://omnivorescookbook.com/wp-content/uploads/2018/05/1805_Mongolian-Beef_550.jpg', 
+            [
+                new Ingredient('beef', 1),
+                new Ingredient('green onions', 2)
+            ])
     ]
 
     getRecipes() {
