b1f21ad011427e14096626a0049fb2aa991a95d6
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-19 15:52:24 -0400

    root routes working and not reload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 
-// import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 // import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 // import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
@@ -10,8 +9,10 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 // import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 const appRoutes: Routes = [
+    { path: '', redirectTo: '/recipes', pathMatch: 'full'},
     { path: 'recipes', component: RecipesComponent },
-    { path: 'shoppinglist', component: ShoppingListComponent }
+    { path: 'shopping-list', component: ShoppingListComponent },
+    // { path: '**', redirectTo: '/recipes', pathMatch: 'full' }
 ]
 
 @NgModule({
