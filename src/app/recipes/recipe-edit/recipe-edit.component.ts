32b006133fd37d44d3070eef2d1d2d89278e8ada
Justina Kuchta <justyna.kuchta@gmail.com>
2020-07-07 13:34:13 -0400

    edit recipe populates all fields including ingredients

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { RecipesService } from '../recipes.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -29,22 +30,39 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = ""
     let recipeImagePath = ""
     let recipeDescription = ""
+    let recipeIngredients = new FormArray([])
 
     if (this.editMode) {
       let recipe = this.recipeService.getRecipe(this.id)
       recipeName = recipe.name
       recipeImagePath = recipe.imagePath
       recipeDescription = recipe.description
+      if (recipe['ingredients']) {
+        for (let ingr of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              // we have two controls here and we need to push them into our recipeIngredients array
+              'name': new FormControl(ingr.name),
+              'amount': new FormControl(ingr.amount)
+            })
+          )
+        }
+      }
     }
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients
     })
   }
 
   onSubmit() {
     console.log(this.recipeForm)
+  }
+
+  get controls() { // a getter!
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
 
 }
