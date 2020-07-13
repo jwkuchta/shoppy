9bfce4bd440668e645abc6f97ca073c2668513e4
Justina Kuchta <justyna.kuchta@gmail.com>
2020-07-13 11:10:07 -0400

    outsourced auth to a selarate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,35 +10,30 @@ import { RecipesService } from './recipes/recipes.service';
 // import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
-import { AuthComponent } from './auth/auth.component';
+// import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-intercepror.service';
 import { AlertComponent } from './shared/alert/alert.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 // import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    DropdownDirective,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    // PlaceholderDirective
+    HeaderComponent
   ],
   // imports other modules into app module
   imports: [
     BrowserModule,
-    FormsModule, 
     AppRoutingModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
-    CoreModule
+    CoreModule,
+    AuthModule
   ],
   // we provide our interceptor that adds user.token to the request here
   // moved providers to core module
